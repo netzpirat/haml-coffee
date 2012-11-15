97450270af1814e21057acee813688d70dde7efc
Michael Kessler <michi@netzpiraten.ch>
2012-11-15 21:52:29 +0100

    Update compiler dist for 1.8.0

--- dist/compiler/hamlcoffee.js
+++ dist/compiler/hamlcoffee.js
@@ -363,7 +363,7 @@ require.define("/haml-coffee.coffee", function (require, module, exports, __dirn
 
   module.exports = HamlCoffee = (function() {
 
-    HamlCoffee.VERSION = '1.7.0';
+    HamlCoffee.VERSION = '1.8.0';
 
     function HamlCoffee(options) {
       var _base, _base10, _base11, _base2, _base3, _base4, _base5, _base6, _base7, _base8, _base9, _ref, _ref10, _ref11, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9;
