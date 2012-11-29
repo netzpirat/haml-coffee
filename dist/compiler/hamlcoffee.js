eea4fe10f2f9c392b48123336840d99ec83910d0
Michael Kessler <michi@netzpiraten.ch>
2012-11-29 11:28:06 +0100

    Update compiler dist for 1.8.2

--- dist/compiler/hamlcoffee.js
+++ dist/compiler/hamlcoffee.js
@@ -363,7 +363,7 @@ require.define("/haml-coffee.coffee", function (require, module, exports, __dirn
 
   module.exports = HamlCoffee = (function() {
 
-    HamlCoffee.VERSION = '1.8.1';
+    HamlCoffee.VERSION = '1.8.2';
 
     function HamlCoffee(options) {
       var _base, _base10, _base11, _base2, _base3, _base4, _base5, _base6, _base7, _base8, _base9, _ref, _ref10, _ref11, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9;
@@ -1299,7 +1299,7 @@ require.define("/nodes/haml.coffee", function (require, module, exports, __dirna
         ch = exp[pos];
         if (ch === '(') {
           level += 1;
-          start = pos;
+          if (level === 1) start = pos;
         }
         if (ch === ')') {
           if (level === 1) {
@@ -1505,11 +1505,10 @@ require.define("/nodes/haml.coffee", function (require, module, exports, __dirna
     };
 
     Haml.prototype.splitInterpolations = function(value) {
-      var ch, ch2, level, pos, quoted, start, tokens, _ref;
+      var ch, ch2, level, pos, start, tokens, _ref;
       level = 0;
       start = 0;
       tokens = [];
-      quoted = false;
       for (pos = 0, _ref = value.length; 0 <= _ref ? pos < _ref : pos > _ref; 0 <= _ref ? pos++ : pos--) {
         ch = value[pos];
         ch2 = value.slice(pos, (pos + 1) + 1 || 9e9);
